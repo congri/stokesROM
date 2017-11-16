75c3f1b0af8e4bbb9ea7f893ac6c27e98fdac32a
Constantin Grigo <constantin.grigo@tum.de>
2017-11-16 12:58:52 +0100

    Changed to >>import dolfin as df<<

--- demo_stokes-iterative.py
+++ demo_stokes-iterative.py
@@ -23,92 +23,91 @@ Note that the sign for the pressure has been flipped for symmetry."""
 
 # Begin demo
 
-from dolfin import *
-
+import dolfin as df
 # Test for PETSc or Epetra
-if not has_linear_algebra_backend("PETSc") and not has_linear_algebra_backend("Epetra"):
-    info("DOLFIN has not been configured with Trilinos or PETSc. Exiting.")
+if not df.has_linear_algebra_backend("PETSc") and not df.has_linear_algebra_backend("Epetra"):
+    df.info("DOLFIN has not been configured with Trilinos or PETSc. Exiting.")
     exit()
 
-if not has_krylov_solver_preconditioner("amg"):
-    info("Sorry, this demo is only available when DOLFIN is compiled with AMG "
+if not df.has_krylov_solver_preconditioner("amg"):
+    df.info("Sorry, this demo is only available when DOLFIN is compiled with AMG "
 	 "preconditioner, Hypre or ML.")
     exit()
 
-if has_krylov_solver_method("minres"):
+if df.has_krylov_solver_method("minres"):
     krylov_method = "minres"
-elif has_krylov_solver_method("tfqmr"):
+elif df.has_krylov_solver_method("tfqmr"):
     krylov_method = "tfqmr"
 else:
-    info("Default linear algebra backend was not compiled with MINRES or TFQMR "
+    df.info("Default linear algebra backend was not compiled with MINRES or TFQMR "
          "Krylov subspace method. Terminating.")
     exit()
 
 # Load mesh
-mesh = UnitCubeMesh(16, 16, 16)
+mesh = df.UnitCubeMesh(16, 16, 16)
 
 # Define function spaces
-V = VectorFunctionSpace(mesh, "CG", 2)
-Q = FunctionSpace(mesh, "CG", 1)
+V = df.VectorFunctionSpace(mesh, "CG", 2)
+Q = df.FunctionSpace(mesh, "CG", 1)
 W = V*Q
 
 # Boundaries
-def right(x, on_boundary): return x[0] > (1.0 - DOLFIN_EPS)
-def left(x, on_boundary): return x[0] < DOLFIN_EPS
+def right(x, on_boundary): return x[0] > (1.0 - df.DOLFIN_EPS)
+def left(x, on_boundary): return x[0] < df.DOLFIN_EPS
 def top_bottom(x, on_boundary):
-    return x[1] > 1.0 - DOLFIN_EPS or x[1] < DOLFIN_EPS
+    return x[1] > 1.0 - df.DOLFIN_EPS or x[1] < df.DOLFIN_EPS
 
 # No-slip boundary condition for velocity
-noslip = Constant((0.0, 0.0, 0.0))
-bc0 = DirichletBC(W.sub(0), noslip, top_bottom)
+noslip = df.Constant((0.0, 0.0, 0.0))
+bc0 = df.DirichletBC(W.sub(0), noslip, top_bottom)
 
 # Inflow boundary condition for velocity
-inflow = Expression(("-sin(x[1]*pi)", "0.0", "0.0"))
-bc1 = DirichletBC(W.sub(0), inflow, right)
+inflow = df.Expression(("-sin(x[1]*pi)", "0.0", "0.0"))
+bc1 = df.DirichletBC(W.sub(0), inflow, right)
 
 # Boundary condition for pressure at outflow
-zero = Constant(0)
-bc2 = DirichletBC(W.sub(1), zero, left)
+zero = df.Constant(0)
+bc2 = df.DirichletBC(W.sub(1), zero, left)
 
 # Collect boundary conditions
 bcs = [bc0, bc1, bc2]
 
 # Define variational problem
-(u, p) = TrialFunctions(W)
-(v, q) = TestFunctions(W)
-f = Constant((0.0, 0.0, 0.0))
-a = inner(grad(u), grad(v))*dx + div(v)*p*dx + q*div(u)*dx
-L = inner(f, v)*dx
+(u, p) = df.TrialFunctions(W)
+(v, q) = df.TestFunctions(W)
+f = df.Constant((0.0, 0.0, 0.0))
+a = df.inner(df.grad(u), df.grad(v))*df.dx + df.div(v)*p*df.dx + q*df.div(u)*df.dx
+L = df.inner(f, v)*df.dx
 
 # Form for use in constructing preconditioner matrix
-b = inner(grad(u), grad(v))*dx + p*q*dx
+b = df.inner(df.grad(u), df.grad(v))*df.dx + p*q*df.dx
 
 # Assemble system
-A, bb = assemble_system(a, L, bcs)
+A, bb = df.assemble_system(a, L, bcs)
 
 # Assemble preconditioner system
-P, btmp = assemble_system(b, L, bcs)
+P, btmp = df.assemble_system(b, L, bcs)
 
 # Create Krylov solver and AMG preconditioner
-solver = KrylovSolver(krylov_method, "amg")
+solver = df.KrylovSolver(krylov_method, "amg")
 
 # Associate operator (A) and preconditioner matrix (P)
 solver.set_operators(A, P)
 
 # Solve
-U = Function(W)
+U = df.Function(W)
 solver.solve(U.vector(), bb)
 
 # Get sub-functions
 u, p = U.split()
 
 # Save solution in VTK format
-ufile_pvd = File("velocity.pvd")
+ufile_pvd = df.File("velocity.pvd")
 ufile_pvd << u
-pfile_pvd = File("pressure.pvd")
+pfile_pvd = df.File("pressure.pvd")
 pfile_pvd << p
 
 # Plot solution
-plot(u)
-plot(p)
-interactive()
+df.plot(u)
+df.plot(p)
+df.interactive()
