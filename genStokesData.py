74c9e55663fcc2ac23652e54043f9d1fa67a7608
Constantin Grigo <constantin.grigo@tum.de>
2017-11-17 20:04:24 +0100

    Running random field

--- genStokesData.py
+++ genStokesData.py
@@ -1,33 +1,14 @@
 """This demo solves the Stokes equations using an iterative linear solver.
 Note that the sign for the pressure has been flipped for symmetry."""
 
-# Copyright (C) 2010 Garth N. Wells
-#
-# This file is part of DOLFIN.
-#
-# DOLFIN is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# DOLFIN is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
-#
-# First added:  2010-08-08
-# Last changed: 2010-08-08
-
-# Begin demo
 
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import dolfin as df
+from randomFieldGeneration import RandomField as rf
+
 # Test for PETSc or Epetra
 if not df.has_linear_algebra_backend("PETSc") and not df.has_linear_algebra_backend("Epetra"):
     df.info("DOLFIN has not been configured with Trilinos or PETSc. Exiting.")
@@ -51,10 +32,12 @@ else:
 
 # Define physical parameters
 mu = 1  # viscosity
+cutoff = 1
 
+randomField = rf.sample(rf)
 
 # Load mesh
-nMesh = 128
+nMesh = 32
 mesh = df.UnitSquareMesh(nMesh, nMesh)
 
 
@@ -70,7 +53,7 @@ def right(x, on_boundary): return x[0] > (1.0 - df.DOLFIN_EPS)
 def left(x, on_boundary): return x[0] < df.DOLFIN_EPS
 def top_bottom(x, on_boundary):
     return x[1] > 1.0 - df.DOLFIN_EPS or x[1] < df.DOLFIN_EPS
-def middle(x, on_boundary): return df.near(x[0], .5, 0.02) and df.near(x[1], .5, 0.2)
+def solidPhase(x, on_boundary): return randomField(x) > cutoff
 
 
 # No-slip boundary condition for velocity
@@ -81,16 +64,19 @@ bc0 = df.DirichletBC(W.sub(0), noslip, top_bottom)
 inflow = df.Expression(("2*sin(2*pi*x[1])", "0.0"), degree=2, mu=mu)
 bc1 = df.DirichletBC(W.sub(0), inflow, right)
 
-# Boundary condition for pressure at outflow
-outflow_p = df.Constant(0)
+# Boundary condition at outflow
 outflow = df.Expression(("sin(2*pi*x[1])", "0.0"), degree=2, mu=mu)
 bc2 = df.DirichletBC(W.sub(0), outflow, left)
 
-noflow = df.Expression(("0", "0.0"), degree=2)
-bc3 = df.DirichletBC(W.sub(0), noflow, middle)
+# Boundary conditions for solid phase
+zero_p = df.Constant(0)
+noflow = df.Expression(("0.0", "0.0"), degree=2)
+bc3 = df.DirichletBC(W.sub(0), noflow, solidPhase)
+# Set pressure to 0 in no flow regions
+bc4 = df.DirichletBC(W.sub(1), zero_p, solidPhase)
 
 # Collect boundary conditions
-bcs = [bc0, bc1, bc2, bc3]
+bcs = [bc0, bc1, bc2, bc3, bc4]
 
 # Define variational problem
 (u, p) = df.TrialFunctions(W)
@@ -121,26 +107,40 @@ solver.solve(U.vector(), bb)
 # Get sub-functions
 u, p = U.split()
 
+# Save solution as text files
 pMesh = p.compute_vertex_values(mesh)
 uMesh = u.compute_vertex_values(mesh)
+print(uMesh.shape)
 x = mesh.coordinates()
-np.savetxt('pressureField', pMesh, delimiter=' ')
-np.savetxt('velocityField', uMesh, delimiter=' ')
-np.savetxt('coordinates', x, delimiter=' ')
+
+cond = np.zeros(x.shape[0])
+for i in range(0, x.shape[0]):
+    cond[i] = randomField(np.transpose(x[i, :]))
+
+cond = np.reshape(cond, [nMesh + 1, nMesh + 1])
+condBin = cond > cutoff
+
+# Plot cond with matplotlib
+fig = plt.figure()
+ax = fig.add_subplot(111)
+cax = ax.imshow(cond, extent=[0, 1, 0, 1])
+cbar = fig.colorbar(cax)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+cax = ax.imshow(condBin, extent=[0, 1, 0, 1])
+cbar = fig.colorbar(cax)
+
+np.savetxt('./data/pressureField', pMesh, delimiter=' ')
+np.savetxt('./data/velocityField', uMesh, delimiter=' ')
+np.savetxt('./data/coordinates', x, delimiter=' ')
 
 
 # Save solution in VTK format
-ufile_pvd = df.File("velocity.pvd")
+ufile_pvd = df.File("./data/velocity.pvd")
 ufile_pvd << u
-pfile_pvd = df.File("pressure.pvd")
+pfile_pvd = df.File("./data/pressure.pvd")
 pfile_pvd << p
-
-"""
-# Plot solution
-df.plot(u)
-df.plot(p)
-df.interactive()
-"""
 
 
 # Plot with matplotlib
@@ -148,6 +148,7 @@ fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 X, Y = np.meshgrid(x[0:(nMesh + 1), 0], x[0:(nMesh + 1), 0])
 pMesh = np.reshape(pMesh, (nMesh + 1, nMesh + 1))
+pMesh[condBin] = np.ma.masked
 
 ax.plot_surface(X, Y, pMesh, cmap=cm.inferno)
 
