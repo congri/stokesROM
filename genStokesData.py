87ae7d37827672065320dfd0f54c154334dd024a
constantin <constantin.grigo@tum.de>
2017-12-01 14:29:47 +0100

    Improved plots, integrated further covariance functions

--- genStokesData.py
+++ genStokesData.py
@@ -8,9 +8,8 @@ import dolfin as df
 from randomFieldGeneration import RandomField as rf
 import mshr
 import time
+import scipy.stats as stats
 from skimage import measure
-
-
 
 
 # Test for PETSc or Epetra
@@ -36,36 +35,31 @@ else:
 
 # Define physical parameters
 mu = 1  # viscosity
-cutoff = 1.0
+volumeFraction = .7
+cutoff = stats.norm.ppf(volumeFraction)
+print('cutoff = ', cutoff)
+nMeshPolygon = 128   # image discretization of random material; needed for polygones
+
+
 
 randomFieldObj = rf()
+randomFieldObj.covarianceFunction = 'sincSq'
 randomField = randomFieldObj.sample()
-'''
-print('Drawing polygones...')
-nMesh = 64
-x = np.linspace(0, 1, nMesh)
-img = np.zeros([nMesh, nMesh])
-for i in range(0, nMesh):
-    for j in range(0, nMesh):
-        img[i, j] = randomField(np.array([x[i], x[j]]))
 
-ic = iso.IsoContour()
-Objects, Vertices, _ = ic.isocontour(img, cutoff)
+print('Discretizing random field...')
+x = np.linspace(0, 1, nMeshPolygon)
+img = np.zeros([nMeshPolygon, nMeshPolygon])
+for i in range(0, nMeshPolygon):
+    for j in range(0, nMeshPolygon):
+        img[i, j] = randomField(np.array([x[i], x[j]]))
 print('done.')
-'''
 
 print('Drawing polygones...')
-nMesh = 128
-x = np.linspace(0, 1, nMesh)
-img = np.zeros([nMesh, nMesh])
-for i in range(0, nMesh):
-    for j in range(0, nMesh):
-        img[i, j] = randomField(np.array([x[i], x[j]]))
-contours = measure.find_contours(img, cutoff)
+contours = measure.find_contours(img, cutoff, positive_orientation='high', fully_connected='high')
 print('done.')
 
 #  Show image
-showImg = True
+showImg = False
 if showImg:
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -75,36 +69,11 @@ if showImg:
         plt.plot(contour[:, 1], contour[:, 0])
 
 
-
 domain = mshr.Rectangle(df.Point(0.0, 0.0), df.Point(1.0, 1.0))
-
-'''
-print('Substracting polygones from domain...')
-for blob in range(0, len(Objects)):
-    polygonVertices = np.flip(Objects[blob], axis=0)
-    # Construct polygon
-    vertexList = []
-    print('Vertices = ', polygonVertices)
-    print('Coordinates = ', Vertices[polygonVertices, :])
-    for i in range(0, polygonVertices.size):
-        # Construct list of df.Point's for polygon vertices
-        vertex = polygonVertices[i]
-        x = np.array([Vertices[vertex, 0], Vertices[vertex, 1]])
-        vertexList.append(df.Point(np.squeeze(x)))
-    # Substract polygon from domain
-    try:
-        domain -= mshr.Polygon(vertexList)
-    except RuntimeError:
-        print('blob = ', blob)
-        print('vertices = ', polygonVertices)
-        print('coordinates = ', Vertices[polygonVertices])
-print('done.')
-'''
-
-print('Substracting polygones from domain...')
+print('Substracting blobs as polygones from domain...')
 for blob in range(0, len(contours)):
     contour = contours[blob]
-    contour = np.flip(contour, axis=0)/(nMesh - 1)
+    contour = contour/(nMeshPolygon - 1)
     vertexList = []
     for i in range(0, contour.shape[0]):
         # Construct list of df.Point's for polygon vertices
@@ -112,24 +81,23 @@ for blob in range(0, len(contours)):
         vertexList.append(df.Point(np.squeeze(x)))
     # Substract polygon from domain
     try:
-        domain -= mshr.Polygon(vertexList)
+        mPolygon = mshr.Polygon(vertexList)
     except RuntimeError:
+        print('Invalid blob at')
         print('blob = ', blob)
         print('contour = ', contour)
+    domain -= mPolygon
 print('done.')
-
 
 print('generating FE mesh...')
 t = time.time()
 mesh = mshr.generate_mesh(domain, 128)
-#mesh = df.UnitSquareMesh(128, 128)
 elapsed_time = time.time() - t
 print('done. Time: ', elapsed_time)
 
 
 
-
-
+print('Setting boundary conditions...')
 # Create classes for defining parts of the boundaries and the interior
 # of the domain
 class DomainBoundary(df.SubDomain):
@@ -158,45 +126,41 @@ upDown = UpDown()
 leftRight = LeftRight()
 solidPhase = RandField()
 
-
-# Initialize mesh function for interior domains
+# Mark subdomains
 domains = df.CellFunction("size_t", mesh)
 domains.set_all(0)
 solidPhase.mark(domains, 1)
 
-# Initialize mesh function for boundary domains
+# Mark boundaries
 boundaries = df.FacetFunction("size_t", mesh)
 boundaries.set_all(0)
 # domainBoundary.mark(boundaries, 1)
 upDown.mark(boundaries, 1)
 leftRight.mark(boundaries, 2)
 
-
-# Define mixed function space
+# Define mixed function space (Taylor-Hood)
 u_e = df.VectorElement("CG", mesh.ufl_cell(), 2)
 p_e = df.FiniteElement("CG", mesh.ufl_cell(), 1)
 mixedEl = df.MixedElement([u_e, p_e])
 W = df.FunctionSpace(mesh, mixedEl)
 
-
 # Flow boundary condition for velocity on domain boundary
-flowFieldLR = df.Expression(('1', '0'), degree=2, mu=mu)
-flowFieldUD = df.Expression(("0.0", "0.0"), degree=2, mu=mu)
-#pressureField = df.Expression("0.0", degree=2, mu=mu)
+flowFieldLR = df.Expression(('0.0', '0.0'), degree=2, mu=mu)
+flowFieldUD = df.Expression(("0.0", "-1.0"), degree=2, mu=mu)
+# pressureField = df.Expression("0.0", degree=2, mu=mu)
 bc1 = df.DirichletBC(W.sub(0), flowFieldLR, leftRight)
 bc2 = df.DirichletBC(W.sub(0), flowFieldUD, upDown)
-#bc3 = df.DirichletBC(W.sub(1), pressureField, domainBoundary)
+# bc3 = df.DirichletBC(W.sub(1), pressureField, domainBoundary)
 
 # No-slip boundary condition for velocity on material interfaces
 noslip = df.Constant((0.0, 0.0))
-zero_p = df.Constant(0.0)
 
 # Boundary conditions for solid phase
 bc4 = df.DirichletBC(W.sub(0), noslip, solidPhase)
-bc5 = df.DirichletBC(W.sub(1), zero_p, solidPhase)
 
 # Collect boundary conditions
 bcs = [bc1, bc2, bc4]
+print('done.')
 
 
 # Define variational problem
@@ -222,55 +186,22 @@ solver = df.KrylovSolver(krylov_method)
 solver.set_operators(A, P)
 
 # Solve
+print('Solving equation system...')
+t = time.time()
 U = df.Function(W)
 solver.solve(U.vector(), bb)
+elapsed_time = time.time() - t
+print('done. Time: ', elapsed_time)
 
 # Get sub-functions
 u, p = U.split()
 
+
 # Save solution as text files
 pMesh = p.compute_vertex_values(mesh)
 uMesh = u.compute_vertex_values(mesh)
-print(uMesh.shape)
+print('uMesh vertices shape: ', uMesh.shape)
 x = mesh.coordinates()
-
-
-'''
-cond = np.zeros([100, 100])
-pMesh = np.zeros([100, 100])
-X = np.zeros([100, 100])
-Y = np.zeros([100, 100])
-for i in range(0, 100):
-    for j in range(0, 100):
-        x = [i/100, j/100]
-        cond[i, j] = randomField(x)
-        pMesh[i, j] = p(x)
-        X[i, j] = x[0]
-        Y[i, j] = x[1]
-
-# cond = np.reshape(cond, [nMesh + 1, nMesh + 1])
-condBin = cond > cutoff
-
-# Plot cond with matplotlib
-fig = plt.figure()
-ax = fig.add_subplot(111)
-cax = ax.contourf(X, Y, cond, extent=[0, 1, 0, 1], cmap=cm.inferno)
-cbar = fig.colorbar(cax)
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-cax = ax.plot_surface(X, Y, pMesh, cmap=cm.inferno)
-cbar = fig.colorbar(cax)
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-cax = ax.contourf(X, Y, condBin, extent=[0, 1, 0, 1], cmap=cm.binary)
-cbar = fig.colorbar(cax)
-'''
-
-
-
-
 
 np.savetxt('./data/pressureField', pMesh, delimiter=' ')
 np.savetxt('./data/velocityField', uMesh, delimiter=' ')
@@ -283,24 +214,6 @@ pfile_pvd = df.File("./data/pressure.pvd")
 pfile_pvd << p
 
 
-'''
-# Plot with matplotlib
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-X, Y = np.meshgrid(x[0:(nMesh + 1), 0], x[0:(nMesh + 1), 0])
-pMesh = np.reshape(pMesh, (nMesh + 1, nMesh + 1))
-# pMesh[condBin] = np.ma.masked
-ax.plot_surface(X, Y, pMesh, cmap=cm.inferno)
-
-# Plot cond with matplotlib
-fig = plt.figure()
-ax = fig.add_subplot(111)
-cax = ax.imshow(pMesh, extent=[0, 1, 0, 1], cmap=cm.inferno)
-cbar = fig.colorbar(cax)
-'''
-
-
-
 
 mx = np.max(pMesh)
 mn = np.min(pMesh)
@@ -308,9 +221,15 @@ fig = plt.figure()
 pp = df.plot(p)
 plt.colorbar(pp)
 fig = plt.figure()
-df.plot(u, cmap=plt.cm.inferno)
-fig = plt.figure()
-df.plot(mesh)
+df.plot(u, cmap=plt.cm.viridis, headwidth=0.005, headlength=0.005, scale=80.0, minlength=0.0001,
+        width=0.0008, minshaft=0.01, headaxislength=0.1)
+for i in range(0, len(contours)):
+    contour = contours[i]/(nMeshPolygon - 1.0)
+    plt.plot(contour[:, 0], contour[:, 1], 'k')
+plt.xticks([])
+plt.yticks([])
+fig.savefig('velocity.pdf')
 
+# df.plot(mesh)
 
 plt.show()
--- randomFieldGeneration.py
+++ randomFieldGeneration.py
@@ -5,67 +5,56 @@ from numpy import random as rnd
 import numpy as np
 
 class RandomField:
-    """se:          squared exponential
-       ou:          Ornstein-Uhlenbeck
-       sinc:        sin(r)/r
-       sincSq:      (sin(r)/r)^2
-       matern:      Matern"""
-    _covarianceFunction = 'se'
-    _lengthScale = [.01, .01]
-    _params = []    # further covariance function parameters
-    _nBasis = 1000  # number of basis functions of truncated random field
-    _sigma = 1      # there should be no need to change this parameter
-
-
-
-    # Setters and getters
-    def set_covarianceFunction(self, cov):
-        assert cov == 'se' or cov == 'ou' or cov == 'sinc' or cov == 'sincSq' or cov == 'matern'
-        self._covarianceFunction = cov
-
-
-    def get_covarianceFunction(self): return self._covarianceFunction
-
-
-    def set_lengthScale(self, l):
-        self._lengthScale = l
-
-
-    def get_lengthScale(self): return self._lengthScale
-
-
-    def set_params(self, params):
-        self._params = params
-
-
-    def get_params(self): return self._params
-
-
-    def set_nBasis(self, nBasis):
-        self._nBasis = nBasis
-
-
-    def get_nBasis(self): return self._nBasis
-
-
+    """
+    se:          squared exponential
+    ou:          Ornstein-Uhlenbeck
+    sinc:        sin(r)/r
+    sincSq:      (sin(r)/r)^2
+    matern:      Matern
+    """
+    covarianceFunction = 'se'
+    lengthScale = [.01, .01]
+    params = [3.0]    # further covariance function parameters
+    nBasis = 1000  # number of basis functions of truncated random field
+    sigma = 1.0      # there should be no need to change this parameter
 
 
     # Draw realization
     def sample(self):
         # Compute stacked samples of W, see references
-        if self._covarianceFunction == 'se':
-            W = rnd.multivariate_normal([0, 0], np.diag(np.power(self._lengthScale, -2)), self._nBasis)
+        if self.covarianceFunction == 'se':
+            # Squared exponential kernel
+            W = rnd.multivariate_normal([0, 0], np.diag(np.power(self.lengthScale, -2)), self.nBasis)
+        elif self.covarianceFunction == 'ou':
+            # Ornstein-Uhlenbeck kernel
+            # modulus
+            W = rnd.standard_t(2.0, (self.nBasis, 1))/self.lengthScale[0]
+            # angle
+            phi = 2*np.pi*rnd.uniform(0, 1, (self.nBasis, 1))
+            W = np.concatenate((W*np.cos(phi), W*np.sin(phi)), axis=1)
+        elif self.covarianceFunction == 'matern':
+            # modulus - 'params' is smoothness parameter nu of Matern kernel
+            W = rnd.standard_t(self.params[0] + .5, (self.nBasis, 1))/self.lengthScale[0]
+            # angle
+            phi = 2*np.pi*rnd.uniform(0, 1, (self.nBasis, 1))
+            W = np.concatenate((W * np.cos(phi), W * np.sin(phi)), axis=1)
+        elif self.covarianceFunction == 'sinc':
+            W = (rnd.uniform(0, 1, (self.nBasis, 1)) - .5)/self.lengthScale[0]
+            W = np.concatenate((W, (rnd.uniform(0, 1, (self.nBasis, 1)) - .5)/self.lengthScale[1]), axis=1)
+        elif self.covarianceFunction == 'sincSq':
+            W = rnd.triangular(-1, 0, 1, (self.nBasis, 1))/self.lengthScale[0]
+            W = np.concatenate((W, rnd.triangular(-1, 0, 1, (self.nBasis, 1))/self.lengthScale[1]), axis=1)
         else:
             raise Exception('Unknown covariance type')
 
         # Compute stacked samples of b, see references
-        b = rnd.uniform(0, 2*np.pi, self._nBasis)
+        b = rnd.uniform(0, 2*np.pi, self.nBasis)
 
         # Draw coefficients gamma, see references
-        gamma = rnd.normal(0, 1, [1, self._nBasis])
+        gamma = rnd.normal(0, 1, [1, self.nBasis])
 
         def samplefun(x):
-            f = np.sqrt((2*self._sigma)/self._nBasis)*(gamma.dot(np.cos(W.dot(x) + b)))
+            f = np.sqrt((2*self.sigma)/self.nBasis)*(gamma.dot(np.cos(W.dot(x) + b)))
             return f
         return samplefun
 
