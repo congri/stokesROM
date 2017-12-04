abc487f872f4a293bb8571a0827cf64da11ce9fd
constantin <constantin.grigo@tum.de>
2017-12-04 16:10:21 +0100

    Added job_file

---
+++ job_file.sh
@@ -0,0 +1,11 @@
+#Job script to generate porous medium meshes for fenics
+
+
+
+
+
+
+#Activate fenics environment for python
+source activate fenics
+/home/constantin/.conda/envs/fenics/bin/python3.6 /home/constantin/python/projects/stokesEquation/generateMeshes.py
+
