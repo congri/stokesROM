11d34fcf4d238b5a760fa3a3c1bfc9b8d52e63aa
constantin <constantin.grigo@tum.de>
2017-12-13 15:59:59 +0100

    plot with points for boundary mesh

--- genStokesData.py
+++ genStokesData.py
@@ -29,13 +29,13 @@ else:
 
 # Define physical parameters
 mu = 1  # viscosity
-volumeFraction = .5
+volumeFraction = .7
 nElements = 128
 nMeshPolygon = 64
 covarianceFunction = 'matern'
 randFieldParams = [5.0]
 lengthScale = [.008, .008]
-meshNumber = 2  #index, which random sample to load
+meshes = np.arange(0, 1)  # vector of random meshes to load
 
 folderbase = '/home/constantin/cluster'
 foldername = folderbase + '/python/data/stokesEquation/meshes/meshSize=' + str(nElements) +\
@@ -43,147 +43,149 @@ foldername = folderbase + '/python/data/stokesEquation/meshes/meshSize=' + str(n
     '/params=' + str(randFieldParams) + '/l=' + str(lengthScale[0]) + '_' +\
     str(lengthScale[1]) + '/volfrac=' + str(volumeFraction)
 
-mesh = df.Mesh(foldername + '/mesh' + str(meshNumber) + '.xml')
-df.plot(mesh)
 
-
-
-
-print('Setting boundary conditions...')
-# Create classes for defining parts of the boundaries and the interior
-# of the domain
+# Set external boundaries of domain
 class DomainBoundary(df.SubDomain):
     def inside(self, x, on_boundary):
         return x[1] > 1.0 - df.DOLFIN_EPS or x[1] < df.DOLFIN_EPS \
-                       or x[0] > (1.0 - df.DOLFIN_EPS) or x[0] < df.DOLFIN_EPS
+               or x[0] > (1.0 - df.DOLFIN_EPS) or x[0] < df.DOLFIN_EPS
+
 
 class UpDown(df.SubDomain):
     def inside(self, x, on_boundary):
         return x[1] > 1.0 - df.DOLFIN_EPS or x[1] < df.DOLFIN_EPS
 
+
 class LeftRight(df.SubDomain):
     def inside(self, x, on_boundary):
         return x[0] > (1.0 - df.DOLFIN_EPS) or x[0] < df.DOLFIN_EPS
 
-class RandField(df.SubDomain):
-    def inside(self, x, on_boundary):
-        outerBoundary = x[1] > 1.0 - df.DOLFIN_EPS or x[1] < df.DOLFIN_EPS \
-            or x[0] > (1.0 - df.DOLFIN_EPS) or x[0] < df.DOLFIN_EPS
-        return on_boundary and not outerBoundary
-
-
-# Initialize sub-domain instances
+# Initialize sub-domain instances for outer domain boundaries
 domainBoundary = DomainBoundary()
 upDown = UpDown()
 leftRight = LeftRight()
-solidPhase = RandField()
-
-# Mark subdomains
-domains = df.CellFunction("size_t", mesh)
-domains.set_all(0)
-solidPhase.mark(domains, 1)
-
-# Mark boundaries
-boundaries = df.FacetFunction("size_t", mesh)
-boundaries.set_all(0)
-# domainBoundary.mark(boundaries, 1)
-upDown.mark(boundaries, 1)
-leftRight.mark(boundaries, 2)
-
-# Define mixed function space (Taylor-Hood)
-u_e = df.VectorElement("CG", mesh.ufl_cell(), 2)
-p_e = df.FiniteElement("CG", mesh.ufl_cell(), 1)
-mixedEl = df.MixedElement([u_e, p_e])
-W = df.FunctionSpace(mesh, mixedEl)
-
-# Flow boundary condition for velocity on domain boundary
-flowFieldLR = df.Expression(('1.0', '0.0'), degree=2, mu=mu)
-flowFieldUD = df.Expression(('0.0', '0.0'), degree=2, mu=mu)
-# pressureField = df.Expression("0.0", degree=2, mu=mu)
-bc1 = df.DirichletBC(W.sub(0), flowFieldLR, leftRight)
-bc2 = df.DirichletBC(W.sub(0), flowFieldUD, upDown)
-# bc3 = df.DirichletBC(W.sub(1), pressureField, domainBoundary)
-
-# No-slip boundary condition for velocity on material interfaces
-noslip = df.Constant((0.0, 0.0))
-
-# Boundary conditions for solid phase
-bc4 = df.DirichletBC(W.sub(0), noslip, solidPhase)
-
-# Collect boundary conditions
-bcs = [bc1, bc2, bc4]
-print('done.')
 
 
-# Define variational problem
-(u, p) = df.TrialFunctions(W)
-(v, q) = df.TestFunctions(W)
-f = df.Constant((0.0, 0.0))
-a = mu*df.inner(df.grad(u), df.grad(v))*df.dx + df.div(v)*p*df.dx + q*df.div(u)*df.dx
-L = df.inner(f, v)*df.dx
+for meshNumber in meshes:
+    # load mesh from file
+    mesh = df.Mesh(foldername + '/mesh' + str(meshNumber) + '.xml')
 
-# Form for use in constructing preconditioner matrix
-b = df.inner(df.grad(u), df.grad(v))*df.dx + p*q*df.dx
+    print('Setting boundary conditions...')
+    # Define interior boundaries
+    class RandField(df.SubDomain):
+        def inside(self, x, on_boundary):
+            outerBoundary = x[1] > 1.0 - df.DOLFIN_EPS or x[1] < df.DOLFIN_EPS \
+                or x[0] > (1.0 - df.DOLFIN_EPS) or x[0] < df.DOLFIN_EPS
+            return on_boundary and not outerBoundary
 
-# Assemble system
-A, bb = df.assemble_system(a, L, bcs)
+    # Initialize sub-domain instance for interior boundaries
+    solidPhase = RandField()
 
-# Assemble preconditioner system
-P, btmp = df.assemble_system(b, L, bcs)
-
-# Create Krylov solver and AMG preconditioner
-solver = df.KrylovSolver(krylov_method)
-
-# Associate operator (A) and preconditioner matrix (P)
-solver.set_operators(A, P)
-
-# Solve
-print('Solving equation system...')
-t = time.time()
-U = df.Function(W)
-solver.solve(U.vector(), bb)
-elapsed_time = time.time() - t
-print('done. Time: ', elapsed_time)
-
-# Get sub-functions
-u, p = U.split()
+    # Mark boundaries
+    boundaries = df.FacetFunction("size_t", mesh)
+    boundaries.set_all(0)
+    # domainBoundary.mark(boundaries, 1)
+    upDown.mark(boundaries, 1)
+    leftRight.mark(boundaries, 2)
 
 
-# Save solution as text files
-pMesh = p.compute_vertex_values(mesh)
-uMesh = u.compute_vertex_values(mesh)
-print('uMesh vertices shape: ', uMesh.shape)
-x = mesh.coordinates()
-
-np.savetxt('./data/pressureField', pMesh, delimiter=' ')
-np.savetxt('./data/velocityField', uMesh, delimiter=' ')
-np.savetxt('./data/coordinates', x, delimiter=' ')
-
-# Save solution in VTK format
-ufile_pvd = df.File("./data/velocity.pvd")
-ufile_pvd << u
-pfile_pvd = df.File("./data/pressure.pvd")
-pfile_pvd << p
+    vertices = df.VertexFunction('size_t', mesh)
+    vertices.set_all(0)
+    solidPhase.mark(vertices, 1)
 
 
+    # Define mixed function space (Taylor-Hood)
+    u_e = df.VectorElement("CG", mesh.ufl_cell(), 2)
+    p_e = df.FiniteElement("CG", mesh.ufl_cell(), 1)
+    mixedEl = df.MixedElement([u_e, p_e])
+    W = df.FunctionSpace(mesh, mixedEl)
 
-mx = np.max(pMesh)
-mn = np.min(pMesh)
-fig = plt.figure()
-pp = df.plot(p)
-plt.colorbar(pp)
-fig = plt.figure()
-df.plot(u, cmap=plt.cm.viridis, headwidth=0.005, headlength=0.005, scale=80.0, minlength=0.0001,
-        width=0.0008, minshaft=0.01, headaxislength=0.1)
-'''
-for i in range(0, len(contours)):
-    contour = contours[i]
-    plt.plot(contour[:, 0], contour[:, 1], 'k')
-'''
-plt.xticks([])
-plt.yticks([])
-fig.savefig('velocity.pdf')
 
-# df.plot(mesh)
+    # Flow boundary condition for velocity on domain boundary
+    flowFieldLR = df.Expression(('1.0', '0.0'), degree=2)
+    flowFieldUD = df.Expression(('0.0', '0.0'), degree=2)
+
+    bc1 = df.DirichletBC(W.sub(0), flowFieldLR, leftRight)
+    bc2 = df.DirichletBC(W.sub(0), flowFieldUD, upDown)
+
+    # No-slip boundary condition for velocity on material interfaces
+    noslip = df.Constant((0.0, 0.0))
+
+    # Boundary conditions for solid phase
+    bc4 = df.DirichletBC(W.sub(0), noslip, solidPhase)
+
+    # Collect boundary conditions
+    bcs = [bc1, bc2, bc4]
+    print('done.')
+
+
+    # Define variational problem
+    (u, p) = df.TrialFunctions(W)
+    (v, q) = df.TestFunctions(W)
+    f = df.Constant((0.0, 0.0)) # right hand side
+    a = mu*df.inner(df.grad(u), df.grad(v))*df.dx + df.div(v)*p*df.dx + q*df.div(u)*df.dx
+    L = df.inner(f, v)*df.dx
+
+    # Form for use in constructing preconditioner matrix
+    b = df.inner(df.grad(u), df.grad(v))*df.dx + p*q*df.dx
+
+    # Assemble system
+    A, bb = df.assemble_system(a, L, bcs)
+
+    # Assemble preconditioner system
+    P, btmp = df.assemble_system(b, L, bcs)
+
+    # Create Krylov solver and AMG preconditioner
+    solver = df.KrylovSolver(krylov_method)
+
+    # Associate operator (A) and preconditioner matrix (P)
+    solver.set_operators(A, P)
+
+    # Solve
+    print('Solving equation system...')
+    t = time.time()
+    U = df.Function(W)
+    solver.solve(U.vector(), bb)
+    elapsed_time = time.time() - t
+    print('done. Time: ', elapsed_time)
+
+    # Get sub-functions
+    u, p = U.split()
+
+    # Save solution as text files
+    pMesh = p.compute_vertex_values(mesh)
+    uMesh = u.compute_vertex_values(mesh)
+    print('uMesh vertices shape: ', uMesh.shape)
+    x = mesh.coordinates()
+
+    np.savetxt('./data/pressureField', pMesh, delimiter=' ')
+    np.savetxt('./data/velocityField', uMesh, delimiter=' ')
+    np.savetxt('./data/coordinates', x, delimiter=' ')
+
+    # Save solution in VTK format
+    ufile_pvd = df.File("./data/velocity.pvd")
+    ufile_pvd << u
+    pfile_pvd = df.File("./data/pressure.pvd")
+    pfile_pvd << p
+
+    df.plot(mesh)
+
+    mx = np.max(pMesh)
+    mn = np.min(pMesh)
+    fig = plt.figure()
+    pp = df.plot(p)
+    plt.colorbar(pp)
+    fig = plt.figure()
+    df.plot(u, cmap=plt.cm.viridis, headwidth=0.005, headlength=0.005, scale=80.0, minlength=0.0001,
+            width=0.0008, minshaft=0.01, headaxislength=0.1)
+    # plot internal boundaries (boundary vertices
+    bmesh = df.BoundaryMesh(mesh, 'exterior')
+    xBoundary = bmesh.coordinates()
+    plt.plot(xBoundary[:, 0], xBoundary[:, 1], 'ko', ms=.5)
+    
+
+    plt.xticks([])
+    plt.yticks([])
+    fig.savefig('velocity.pdf')
 
 plt.show()
